af3e94ce185e76ec70827820232e738d966589bf
Dmitry Lavrenov <39522748+dmitrylavrenov@users.noreply.github.com>
2022-07-25 16:19:23 +0300

    Add command to subsquid-node (#10)
    * Add command to subsquid-node
    
    * Fix typo

--- crates/subsquid-node/src/main.rs
+++ crates/subsquid-node/src/main.rs
@@ -1,9 +1,10 @@
 mod benchmarking;
 mod chain_spec;
 mod cli;
+mod command;
 #[macro_use]
 mod service;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> sc_cli::Result<()> {
+    command::run()
 }
