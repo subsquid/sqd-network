9319da3e3f71223395115721e281de2d6055effe
Dmitry Lavrenov <lawrenowdima@gmail.com>
2022-09-09 00:07:23 +0300

    Implemet WeightInfo for ()

--- crates/pallet-archive/src/weights.rs
+++ crates/pallet-archive/src/weights.rs
@@ -1,6 +1,13 @@
 use frame_support::weights::Weight;
+use sp_runtime::traits::Zero;
 
 /// Weight functions needed for pallet-archive.
 pub trait WeightInfo {
     fn register() -> Weight;
+}
+
+impl WeightInfo for () {
+    fn register() -> Weight {
+        Weight::zero()
+    }
 }
