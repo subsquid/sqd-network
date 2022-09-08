760892e3829f7571abd73151d4f91d0ccf0d7b0a
Dmitry Lavrenov <lawrenowdima@gmail.com>
2022-09-09 01:24:45 +0300

    Implement tests

--- crates/pallet-archive/src/tests.rs
+++ crates/pallet-archive/src/tests.rs
@@ -1,19 +1,21 @@
 //! Tests.
 
-use crate::{self as pallet_archive};
+use crate::{self as pallet_archive, *};
 use frame_support::{
-    assert_noop, assert_ok, ord_parameter_types, parameter_types,
+    assert_noop, assert_ok,
     traits::{ConstU32, ConstU64},
 };
 use sp_core::H256;
 use sp_runtime::{
     testing::{Header, UintAuthorityId},
-    traits::{BadOrigin, BlakeTwo256, IdentityLookup},
+    traits::{BlakeTwo256, IdentityLookup},
     BuildStorage,
 };
 
 type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
 type Block = frame_system::mocking::MockBlock<Test>;
+
+const MAX_ARCHIVES: u32 = 100;
 
 frame_support::construct_runtime!(
     pub enum Test where
@@ -56,7 +58,7 @@ impl frame_system::Config for Test {
 impl pallet_archive::Config for Test {
     type Event = Event;
     type ArchiveId = UintAuthorityId;
-    type MaxArchives = ConstU32<100>;
+    type MaxArchives = ConstU32<MAX_ARCHIVES>;
     type WeightInfo = ();
 }
 
@@ -68,4 +70,38 @@ pub fn new_test_ext() -> sp_io::TestExternalities {
 pub fn new_test_ext_with(genesis_config: GenesisConfig) -> sp_io::TestExternalities {
     let storage = genesis_config.build_storage().unwrap();
     storage.into()
+}
+
+#[test]
+fn archive_registration_works() {
+    new_test_ext().execute_with(|| {
+        // Verify test preconditions.
+        assert_eq!(Archive::archives(), vec![]);
+
+        // Execute register call.
+        assert_ok!(Archive::register(Origin::signed(1), 100.into()));
+
+        // Ensure that the archive has been added.
+        assert_eq!(Archive::archives(), vec![100.into()]);
+    });
+}
+
+#[test]
+fn archive_registration_already_registered_error() {
+    new_test_ext().execute_with(|| {
+        // Prepare test preconditions.
+        let archives_before = vec![UintAuthorityId::from(100)];
+        <Archives<Test>>::put(
+            WeakBoundedVec::<_, ConstU32<MAX_ARCHIVES>>::try_from(archives_before.clone()).unwrap(),
+        );
+
+        // Execute register call.
+        assert_noop!(
+            Archive::register(Origin::signed(1), 100.into()),
+            Error::<Test>::ArchiveAlreadyRegistered
+        );
+
+        // Ensure that the archive hasn't been added.
+        assert_eq!(Archive::archives(), archives_before);
+    });
 }
