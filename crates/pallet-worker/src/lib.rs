759d0054dc7608d22eef9eb9a4a00d1d93fc17ee
Dmitry Lavrenov <lawrenowdima@gmail.com>
2022-10-22 11:55:23 +0400

    Add simple logic to pallet-worker

--- crates/pallet-worker/src/lib.rs
+++ crates/pallet-worker/src/lib.rs
@@ -22,7 +22,7 @@ pub mod pallet {
         /// Event type.
         type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
         /// WorkerId type.
-        type WorkerId: Parameter + MaxEncodedLen;
+        type WorkerId: Parameter + MaxEncodedLen + Copy;
         /// WeightInfo type that should implement `WeightInfo` trait.
         type WeightInfo: WeightInfo;
     }
@@ -44,31 +44,52 @@ pub mod pallet {
         RunTask { task: Task },
     }
 
+    #[pallet::error]
+    pub enum Error<T> {
+        NoWorkerId,
+        NoAssignedTask,
+    }
+
     #[pallet::call]
     impl<T: Config> Pallet<T> {
         /// Executte register call by WorkerCandidate.
         #[pallet::weight(T::WeightInfo::register())]
-        pub fn register(_origin: OriginFor<T>, worker_id: T::WorkerId) -> DispatchResult {
+        pub fn register(origin: OriginFor<T>, worker_id: T::WorkerId) -> DispatchResult {
+            let _ = ensure_signed(origin);
+
             Self::deposit_event(Event::NewWorker { worker_id });
-            todo!()
+
+            <Workers<T>>::insert(worker_id, Status::Ready);
+
+            Ok(())
         }
 
-        /// Execute submit_task_result call by Worker.
         #[pallet::weight(T::WeightInfo::register())]
-        pub fn done(_origin: OriginFor<T>, _task: Task) -> DispatchResult {
-            todo!()
+        pub fn done(_origin: OriginFor<T>, worker_id: T::WorkerId) -> DispatchResult {
+            let current_status = Self::current_status(worker_id)?;
+
+            if current_status == Status::Ready {
+                return Err(<Error<T>>::NoAssignedTask.into());
+            }
+
+            <Workers<T>>::insert(worker_id, Status::Ready);
+
+            Ok(())
         }
     }
 
     impl<T: Config> Pallet<T> {
-        fn _current_status(
-            _worker_id: T::WorkerId,
+        pub fn current_status(
+            worker_id: T::WorkerId,
         ) -> sp_std::result::Result<Status, DispatchError> {
-            todo!()
+            let status = <Workers<T>>::get(worker_id).ok_or(<Error<T>>::NoWorkerId)?;
+            Ok(status)
         }
 
-        fn _run_task(_worker_id: T::WorkerId, _task: Task) -> DispatchResult {
-            todo!()
+        pub fn run_task(worker_id: T::WorkerId, task: Task) -> DispatchResult {
+            <Workers<T>>::insert(worker_id, Status::Run(task));
+
+            Ok(())
         }
     }
 }
