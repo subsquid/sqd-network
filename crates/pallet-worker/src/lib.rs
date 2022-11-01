270298111e1dbd36a5fd290a5ebc0815c8ea35ad
Dmitry Lavrenov <lawrenowdima@gmail.com>
2022-11-01 13:05:09 +0400

    Fix pallet-worker logic

--- crates/pallet-worker/src/lib.rs
+++ crates/pallet-worker/src/lib.rs
@@ -3,7 +3,6 @@
 // Ensure we're `no_std` when compiling for Wasm.
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use codec::MaxEncodedLen;
 use primitives_worker::{Status, Task};
 
 pub use pallet::*;
@@ -21,8 +20,6 @@ pub mod pallet {
     pub trait Config: frame_system::Config {
         /// Event type.
         type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
-        /// WorkerId type.
-        type WorkerId: Parameter + MaxEncodedLen + Copy;
         /// WeightInfo type that should implement `WeightInfo` trait.
         type WeightInfo: WeightInfo;
     }
@@ -31,48 +28,57 @@ pub mod pallet {
     #[pallet::generate_store(pub(super) trait Store)]
     pub struct Pallet<T>(_);
 
-    /// Current workers state in `StorageMap` view: `WorkerId` -> `Status`.
     #[pallet::storage]
     #[pallet::getter(fn workers)]
-    pub type Workers<T: Config> = StorageMap<_, Twox64Concat, T::WorkerId, Status, OptionQuery>;
+    pub type Workers<T: Config> = StorageMap<_, Twox64Concat, T::AccountId, Status, OptionQuery>;
 
     /// Possible events list.
     #[pallet::event]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
     pub enum Event<T: Config> {
-        NewWorker { worker_id: T::WorkerId },
-        RunTask { task: Task },
+        NewWorker { worker_id: T::AccountId },
+        RunTask { worker_id: T::AccountId, task: Task },
+        TaskDone { worker_id: T::AccountId, task: Task },
     }
 
     #[pallet::error]
     pub enum Error<T> {
         NoWorkerId,
-        NoAssignedTask,
+        NoSubmittedTask,
+        WorkerIsBusy,
     }
 
     #[pallet::call]
     impl<T: Config> Pallet<T> {
-        /// Executte register call by WorkerCandidate.
         #[pallet::weight(T::WeightInfo::register())]
-        pub fn register(origin: OriginFor<T>, worker_id: T::WorkerId) -> DispatchResult {
-            let _ = ensure_signed(origin);
+        pub fn register(origin: OriginFor<T>) -> DispatchResult {
+            let who = ensure_signed(origin)?;
 
-            Self::deposit_event(Event::NewWorker { worker_id });
+            Self::deposit_event(Event::NewWorker {
+                worker_id: who.clone(),
+            });
 
-            <Workers<T>>::insert(worker_id, Status::Ready);
+            <Workers<T>>::insert(who, Status::Ready);
 
             Ok(())
         }
 
         #[pallet::weight(T::WeightInfo::done())]
-        pub fn done(_origin: OriginFor<T>, worker_id: T::WorkerId) -> DispatchResult {
-            let current_status = Self::current_status(worker_id)?;
+        pub fn done(origin: OriginFor<T>, task: Task) -> DispatchResult {
+            let who = ensure_signed(origin)?;
 
-            if current_status == Status::Ready {
-                return Err(<Error<T>>::NoAssignedTask.into());
+            let current_status = Self::current_status(who.clone())?;
+
+            if current_status != Status::Run(task.clone()) {
+                return Err(<Error<T>>::NoSubmittedTask.into());
             }
 
-            <Workers<T>>::insert(worker_id, Status::Ready);
+            Self::deposit_event(Event::TaskDone {
+                worker_id: who.clone(),
+                task,
+            });
+
+            <Workers<T>>::insert(who, Status::Ready);
 
             Ok(())
         }
@@ -80,15 +86,25 @@ pub mod pallet {
 
     impl<T: Config> Pallet<T> {
         pub fn current_status(
-            worker_id: T::WorkerId,
+            worker_id: T::AccountId,
         ) -> sp_std::result::Result<Status, DispatchError> {
             let status = <Workers<T>>::get(worker_id).ok_or(<Error<T>>::NoWorkerId)?;
             Ok(status)
         }
 
-        pub fn run_task(worker_id: T::WorkerId, task: Task) -> DispatchResult {
-            <Workers<T>>::insert(worker_id, Status::Run(task));
+        pub fn run_task(worker_id: T::AccountId, task: Task) -> DispatchResult {
+            let current_status = Self::current_status(worker_id.clone())?;
 
+            if current_status != Status::Ready {
+                return Err(<Error<T>>::WorkerIsBusy.into());
+            }
+
+            Self::deposit_event(Event::RunTask {
+                worker_id: worker_id.clone(),
+                task: task.clone(),
+            });
+
+            <Workers<T>>::insert(worker_id, Status::Run(task));
             Ok(())
         }
     }
