7e82a2e9784db65ad4d5ba284da17f4dc39c3348
Dmitry Lavrenov <lawrenowdima@gmail.com>
2022-12-02 16:57:04 +0300

    Add pallet-workers docs

--- crates/pallet-worker/src/lib.rs
+++ crates/pallet-worker/src/lib.rs
@@ -21,10 +21,13 @@ pub mod pallet {
     pub trait Config: frame_system::Config {
         /// Event type.
         type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+        /// Worker task type.
         type Task: Parameter + MaxEncodedLen + Default;
+        /// Task result type.
         type Result: Parameter + MaxEncodedLen;
+        /// The type that to update task related request.
         type UpdateRequestStatus: UpdateRequestStatus<Task = Self::Task, Result = Self::Result>;
-        /// WeightInfo type that should implement `WeightInfo` trait.
+        /// The weight information provider type.
         type WeightInfo: WeightInfo;
     }
 
@@ -32,6 +35,7 @@ pub mod pallet {
     #[pallet::generate_store(pub(super) trait Store)]
     pub struct Pallet<T>(_);
 
+    /// The list of available workers in the network.
     #[pallet::storage]
     #[pallet::getter(fn workers)]
     pub type Workers<T: Config> = StorageMap<_, Twox64Concat, T::AccountId, T::Task, OptionQuery>;
@@ -40,30 +44,43 @@ pub mod pallet {
     #[pallet::event]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
     pub enum Event<T: Config> {
+        /// New worker has been registered in the network.
         NewWorker {
+            /// The worker id.
             worker_id: T::AccountId,
         },
+        /// The new task has been assigned for worker.
         RunTask {
+            /// Worker id the task has been assigned to.
             worker_id: T::AccountId,
+            /// The task that has been assigned.
             task: T::Task,
         },
+        /// The task has been done.
         TaskDone {
+            /// The worker of finished task.
             worker_id: T::AccountId,
+            /// The task information.
             task: T::Task,
+            /// The task result.
             result: T::Result,
         },
     }
 
     #[pallet::error]
     pub enum Error<T> {
+        /// No worker was found.
         NoWorkerId,
+        /// No submitted task was found.
         NoSubmittedTask,
+        /// The worker is busy with another task.
         WorkerIsBusy,
     }
 
     #[pallet::call]
     impl<T: Config> Pallet<T> {
         #[pallet::weight(T::WeightInfo::register())]
+        /// Register the worker in the network.
         pub fn register(origin: OriginFor<T>) -> DispatchResult {
             let who = ensure_signed(origin)?;
 
@@ -77,6 +94,7 @@ pub mod pallet {
         }
 
         #[pallet::weight(T::WeightInfo::done())]
+        /// Submit the task result.
         pub fn done(origin: OriginFor<T>, task: T::Task, result: T::Result) -> DispatchResult {
             let who = ensure_signed(origin)?;
 
@@ -101,13 +119,13 @@ pub mod pallet {
     }
 
     impl<T: Config> Pallet<T> {
-        pub fn current_task(
-            worker_id: T::AccountId,
-        ) -> sp_std::result::Result<T::Task, DispatchError> {
+        /// A helper function to get current task of the worker.
+        fn current_task(worker_id: T::AccountId) -> sp_std::result::Result<T::Task, DispatchError> {
             let task = <Workers<T>>::get(worker_id).ok_or(<Error<T>>::NoWorkerId)?;
             Ok(task)
         }
 
+        /// Assign the task to particular worker.
         pub fn run_task(worker_id: T::AccountId, task: T::Task) -> DispatchResult {
             let current_task = Self::current_task(worker_id.clone())?;
 
--- crates/pallet-worker/src/traits.rs
+++ crates/pallet-worker/src/traits.rs
@@ -1,8 +1,14 @@
+//! Traits that pallet uses.
+
 use sp_runtime::DispatchResult;
 
+/// An interface to update request status.
 pub trait UpdateRequestStatus {
+    /// The task type.
     type Task;
+    /// The result type.
     type Result;
 
+    /// Update request status.
     fn update_request_status(task: Self::Task, result: Self::Result) -> DispatchResult;
 }
