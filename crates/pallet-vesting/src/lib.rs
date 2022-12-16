3e239023469f56582a6fcddeb341020428e7c323
Dmitry Lavrenov <lawrenowdima@gmail.com>
2022-12-16 13:21:38 +0100

    Working vested transfer

--- crates/pallet-vesting/src/lib.rs
+++ crates/pallet-vesting/src/lib.rs
@@ -4,9 +4,8 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use codec::{Decode, Encode, MaxEncodedLen};
-use frame_support::traits::{Currency, LockableCurrency, WithdrawReasons};
+use frame_support::traits::{Currency, LockIdentifier, LockableCurrency, WithdrawReasons};
 use scale_info::TypeInfo;
-use sp_runtime::traits::StaticLookup;
 
 pub use pallet::*;
 pub use weights::WeightInfo;
@@ -16,23 +15,25 @@ pub mod weights;
 type BalanceOf<T> =
     <<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
 
-type AccountIdLookupOf<T> = <<T as frame_system::Config>::Lookup as StaticLookup>::Source;
+const VESTING_ID: LockIdentifier = *b"vesting ";
 
 /// Struct to encode the vesting schedule of an individual account.
 #[derive(Encode, Decode, Debug, Copy, Clone, PartialEq, Eq, MaxEncodedLen, TypeInfo)]
-pub struct VestingInfo<Balance, BlockNumber> {
+pub struct VestingInfo<Balance, VestingTime> {
     /// Locked amount at genesis.
     locked: Balance,
-    /// Amount that gets unlocked every block after `starting_block`.
-    per_block: Balance,
-    /// Starting block for unlocking(vesting).
-    starting_block: BlockNumber,
+    /// Cliff.
+    cliff: VestingTime,
+    /// Duration.
+    duration: VestingTime,
 }
 
 #[frame_support::pallet]
 pub mod pallet {
     use super::*;
-    use frame_support::pallet_prelude::*;
+    use frame_support::{
+        pallet_prelude::*, storage::with_storage_layer, traits::ExistenceRequirement,
+    };
     use frame_system::pallet_prelude::*;
 
     #[pallet::config]
@@ -47,15 +48,15 @@ pub mod pallet {
         #[pallet::constant]
         type MinVestedTransfer: Get<BalanceOf<Self>>;
 
+        /// The vesting time representation.
+        type VestingTime: Parameter + MaxEncodedLen;
+
         /// Weight information for extrinsics in this pallet.
         type WeightInfo: WeightInfo;
 
         /// Reasons that determine under which conditions the balance may drop below
         /// the unvested amount.
         type UnvestedFundsAllowedWithdrawReasons: Get<WithdrawReasons>;
-
-        /// Maximum number of vesting schedules an account may have at a given moment.
-        const MAX_VESTING_SCHEDULES: u32;
     }
 
     #[pallet::pallet]
@@ -66,13 +67,24 @@ pub mod pallet {
     #[pallet::storage]
     #[pallet::getter(fn vesting)]
     pub type Vesting<T: Config> =
-        StorageMap<_, Blake2_128Concat, T::AccountId, VestingInfo<BalanceOf<T>, T::BlockNumber>>;
+        StorageMap<_, Blake2_128Concat, T::AccountId, VestingInfo<BalanceOf<T>, T::VestingTime>>;
 
     #[pallet::event]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
     pub enum Event<T: Config> {
         /// An \[account\] has become fully vested.
         VestingCompleted { account: T::AccountId },
+    }
+
+    /// Error for the vesting pallet.
+    #[pallet::error]
+    pub enum Error<T> {
+        /// The account given is not vesting.
+        NotVesting,
+        /// Amount being transferred is too low to create a vesting schedule.
+        AmountLow,
+        /// The account used to create vesting is already used.
+        AccountAlreadyHasVesting,
     }
 
     #[pallet::call]
@@ -94,9 +106,8 @@ pub mod pallet {
         /// - `target`: The account whose vested funds should be unlocked. Must have funds still
         /// locked under this pallet.
         #[pallet::weight(T::WeightInfo::vest_locked())]
-        pub fn vest_other(origin: OriginFor<T>, target: AccountIdLookupOf<T>) -> DispatchResult {
-            ensure_signed(origin)?;
-            let who = T::Lookup::lookup(target)?;
+        pub fn vest_other(origin: OriginFor<T>, target: T::AccountId) -> DispatchResult {
+            let who = ensure_signed(origin)?;
             todo!()
         }
 
@@ -111,37 +122,42 @@ pub mod pallet {
         #[pallet::weight(T::WeightInfo::vest_locked())]
         pub fn vested_transfer(
             origin: OriginFor<T>,
-            target: AccountIdLookupOf<T>,
-            schedule: VestingInfo<BalanceOf<T>, T::BlockNumber>,
+            target: T::AccountId,
+            schedule: VestingInfo<BalanceOf<T>, T::VestingTime>,
         ) -> DispatchResult {
             let transactor = ensure_signed(origin)?;
-            let transactor = <T::Lookup as StaticLookup>::unlookup(transactor);
-            todo!()
-        }
 
-        /// Force a vested transfer.
-        ///
-        /// The dispatch origin for this call must be _Root_.
-        ///
-        /// - `source`: The account whose funds should be transferred.
-        /// - `target`: The account that should be transferred the vested funds.
-        /// - `schedule`: The vesting schedule attached to the transfer.
-        ///
-        /// Emits `VestingCreated`.
-        #[pallet::weight(T::WeightInfo::vest_locked())]
-        pub fn force_vested_transfer(
-            origin: OriginFor<T>,
-            source: AccountIdLookupOf<T>,
-            target: AccountIdLookupOf<T>,
-            schedule: VestingInfo<BalanceOf<T>, T::BlockNumber>,
-        ) -> DispatchResult {
-            ensure_root(origin)?;
-            todo!()
+            ensure!(
+                schedule.locked >= T::MinVestedTransfer::get(),
+                Error::<T>::AmountLow
+            );
+
+            if <Vesting<T>>::contains_key(&target) {
+                return Err(<Error<T>>::AccountAlreadyHasVesting.into());
+            }
+
+            with_storage_layer(move || {
+                T::Currency::transfer(
+                    &transactor,
+                    &target,
+                    schedule.locked,
+                    ExistenceRequirement::KeepAlive,
+                )?;
+
+                let reasons =
+                    WithdrawReasons::except(T::UnvestedFundsAllowedWithdrawReasons::get());
+
+                T::Currency::set_lock(VESTING_ID, &target, schedule.locked, reasons);
+
+                <Vesting<T>>::insert(target, schedule);
+
+                Ok(())
+            })
         }
 
         /// Remove vesting (sudo or owner)?
         #[pallet::weight(T::WeightInfo::vest_locked())]
-        pub fn remove_vest(origin: OriginFor<T>, target: AccountIdLookupOf<T>) -> DispatchResult {
+        pub fn remove_vest(origin: OriginFor<T>, target: T::AccountId) -> DispatchResult {
             ensure_root(origin)?;
             todo!()
         }
