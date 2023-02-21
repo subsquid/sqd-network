aface2e7a920111268b72c69087f68084e35980a
Adam Wierzbicki <a.wierzbicki@subsquid.io>
2023-02-21 17:11:07 +0100

    Bindings & adapters for sql-archives worker integration

--- grpc-libp2p/src/lib.rs
+++ grpc-libp2p/src/lib.rs
@@ -1,5 +1,6 @@
 #![feature(is_some_and)]
 
+use cxx::{CxxString, CxxVector, UniquePtr};
 use libp2p::{
     kad::{BootstrapError, NoKnownPeers},
     swarm::DialError,
@@ -38,5 +39,54 @@ impl From<DialError> for Error {
 impl From<&DialError> for Error {
     fn from(err: &DialError) -> Self {
         Self::Dial(format!("{err:?}"))
+    }
+}
+
+pub struct P2PSender {}
+
+impl P2PSender {
+    pub fn send_message(&mut self, peer_id: &CxxString, msg: UniquePtr<CxxVector<u8>>) {
+        let msg = String::from_utf8_lossy(msg.as_ref().unwrap().as_slice());
+        println!("Sending message '{msg}' to peer {peer_id:?}")
+    }
+}
+
+#[cxx::bridge(namespace = "subsquid")]
+pub mod ffi {
+    extern "Rust" {
+        type P2PSender;
+        #[cxx_name = "sendMessage"]
+        fn send_message(&mut self, peer_id: &CxxString, msg: UniquePtr<CxxVector<u8>>);
+    }
+
+    unsafe extern "C++" {
+        include!("grpc-libp2p/sql-archives/worker/src/rust_binding.hpp");
+        include!("grpc-libp2p/src/worker/worker.hpp");
+
+        #[rust_name = "new_buffer"]
+        fn newBuffer(size: usize) -> UniquePtr<CxxVector<u8>>;
+
+        type MessageReceiver;
+        #[rust_name = "on_message_received"]
+        fn onMessageReceived(
+            self: Pin<&mut MessageReceiver>,
+            peer_id: &CxxString,
+            msg: UniquePtr<CxxVector<u8>>,
+        );
+
+        type MessageSender;
+        #[rust_name = "wrap_sender"]
+        fn wrapSender(sender: Box<P2PSender>) -> UniquePtr<MessageSender>;
+
+        type Worker;
+        #[rust_name = "new_worker"]
+        fn newWorker() -> UniquePtr<Worker>;
+        fn configure(self: Pin<&mut Worker>, opaque: &CxxString);
+        fn start(
+            self: Pin<&mut Worker>,
+            sender: UniquePtr<MessageSender>,
+        ) -> UniquePtr<MessageReceiver>;
+        fn stop(self: Pin<&mut Worker>);
+
     }
 }
---
+++ grpc-libp2p/src/worker/worker.cpp
@@ -0,0 +1,30 @@
+#include "grpc-libp2p/src/worker/worker.hpp"
+
+namespace subsquid {
+
+    class WrappedP2PSender: public MessageSender {
+        private:
+            rust::Box<P2PSender> inner;
+        public:
+            WrappedP2PSender() = delete;
+            WrappedP2PSender(rust::Box<P2PSender> inner) : inner(std::move(inner)) {};
+            void sendMessage(const std::string& peerId, std::unique_ptr<Buffer> msg) noexcept {
+                inner->sendMessage(peerId, std::move(msg));
+            }
+    };
+
+    std::unique_ptr<Buffer> newBuffer(size_t size) {
+        auto buf = std::make_unique<Buffer>();
+        buf->reserve(size);
+        return buf;
+    }
+
+    std::unique_ptr<Worker> newWorker() {
+        return std::make_unique<Worker>();
+    }
+
+    std::unique_ptr<MessageSender> wrapSender(rust::Box<P2PSender> sender) {
+        return std::make_unique<WrappedP2PSender>(std::move(sender));
+    }
+
+}
