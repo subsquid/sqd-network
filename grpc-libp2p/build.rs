11f39cae1a41ce4b800006cf9a1f75ce47abe239
Adam Wierzbicki <a.wierzbicki@subsquid.io>
2023-03-20 17:10:00 +0100

    Use worker as a static library

--- grpc-libp2p/build.rs
+++ grpc-libp2p/build.rs
@@ -10,8 +10,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let cpp_dst =
             cmake::Config::new("./sql-archives/worker").build_target("RustBinding").build();
         println!("cargo:rustc-link-search={}/build/src", cpp_dst.display());
-        println!("cargo:rustc-link-lib=RustBinding");
+        println!("cargo:rustc-link-lib=static=RustBinding");
         println!("cargo:rerun-if-changed=sql-archives/worker");
+
+        // For some reason linker fails if these worker dependencies are not included directly,
+        // even though all relevant symbols should be included in libRustBinding
+        let (fmt, spdlog) = match std::env::var("PROFILE").unwrap().as_str() {
+            "release" => ("fmt", "spdlog"),
+            "debug" => ("fmtd", "spdlogd"),
+            x => panic!("Unexpected compilation profile: {x}"),
+        };
+        println!("cargo:rustc-link-search={}/build/_deps/fmt-build", cpp_dst.display());
+        println!("cargo:rustc-link-lib=static={fmt}");
+        println!("cargo:rustc-link-search={}/build/_deps/spdlog-build", cpp_dst.display());
+        println!("cargo:rustc-link-lib=static={spdlog}");
 
         cxx_build::bridge("src/worker.rs")
             .file("src/worker/worker.cpp")
