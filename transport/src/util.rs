8440a22573fbe44d540b2b6e78a3dfd5cc6396c9
Adam Wierzbicki <a.wierzbicki@subsquid.io>
2023-05-24 14:08:43 +0200

    Fixed generating new network keys

--- transport/src/util.rs
+++ transport/src/util.rs
@@ -10,19 +10,22 @@ pub async fn get_keypair(path: Option<PathBuf>) -> anyhow::Result<Keypair> {
         Some(path) => path,
         None => return Ok(Keypair::generate_ed25519()),
     };
-    let meta = tokio::fs::metadata(&path).await?;
-    if meta.is_file() {
-        log::info!("Reading key from {}", path.display());
-        let mut content = tokio::fs::read(&path).await?;
-        let keypair = ed25519::Keypair::decode(content.as_mut_slice())?;
-        Ok(Keypair::Ed25519(keypair))
-    } else if meta.is_dir() {
-        anyhow::bail!("Path exists and is not a file")
-    } else {
-        log::info!("Generating new key and saving into {}", path.display());
-        let keypair = ed25519::Keypair::generate();
-        tokio::fs::write(&path, keypair.encode()).await?;
-        Ok(Keypair::Ed25519(keypair))
+    match tokio::fs::metadata(&path).await {
+        Ok(meta) if meta.is_file() => {
+            log::info!("Reading key from {}", path.display());
+            let mut content = tokio::fs::read(&path).await?;
+            let keypair = ed25519::Keypair::decode(content.as_mut_slice())?;
+            Ok(Keypair::Ed25519(keypair))
+        }
+        Ok(_) => {
+            anyhow::bail!("Path exists and is not a file")
+        }
+        Err(_) => {
+            log::info!("Generating new key and saving into {}", path.display());
+            let keypair = ed25519::Keypair::generate();
+            tokio::fs::write(&path, keypair.encode()).await?;
+            Ok(Keypair::Ed25519(keypair))
+        }
     }
 }
 
