0d10fae97991919c22db9e2f91731e0652f12277
Adam Wierzbicki <a.wierzbicki@subsquid.io>
2023-09-19 15:49:49 +0200

    Allow private addresses in testing environment

--- transport/src/util.rs
+++ transport/src/util.rs
@@ -33,9 +33,41 @@ pub async fn get_keypair(path: Option<PathBuf>) -> anyhow::Result<Keypair> {
 pub fn addr_is_reachable(addr: &Multiaddr) -> bool {
     match addr.iter().next() {
         Some(Protocol::Ip4(addr)) => {
-            !(addr.is_loopback() || addr.is_private() || addr.is_link_local())
+            !(addr.is_loopback() || addr.is_link_local())
+            // We need to allow private addresses for testing in local environment
+            &&(!addr.is_private() || std::env::var("PRIVATE_NETWORK").is_ok())
         }
         Some(Protocol::Ip6(addr)) => !addr.is_loopback(),
+        Some(Protocol::Dns(_)) => true,
+        Some(Protocol::Dns4(_)) => true,
+        Some(Protocol::Dns6(_)) => true,
+        Some(Protocol::Dnsaddr(_)) => true,
         _ => false,
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use libp2p::multiaddr::multiaddr;
+
+    #[test]
+    fn test_addr_is_reachable() {
+        assert!(!addr_is_reachable(&multiaddr!(Ip4([127, 0, 0, 1]), Tcp(12345u16))));
+        assert!(!addr_is_reachable(&multiaddr!(Ip4([169, 254, 0, 1]), Tcp(12345u16))));
+        assert!(!addr_is_reachable(&multiaddr!(Ip4([192, 168, 0, 1]), Tcp(12345u16))));
+        assert!(!addr_is_reachable(&multiaddr!(Ip4([10, 0, 0, 1]), Tcp(12345u16))));
+        assert!(!addr_is_reachable(&multiaddr!(Ip4([172, 16, 0, 1]), Tcp(12345u16))));
+        assert!(!addr_is_reachable(&multiaddr!(Ip6([0, 0, 0, 0, 0, 0, 0, 1]), Tcp(12345u16))));
+
+        std::env::set_var("PRIVATE_NETWORK", "1");
+
+        assert!(!addr_is_reachable(&multiaddr!(Ip4([127, 0, 0, 1]), Tcp(12345u16))));
+        assert!(!addr_is_reachable(&multiaddr!(Ip4([169, 254, 0, 1]), Tcp(12345u16))));
+        assert!(!addr_is_reachable(&multiaddr!(Ip6([0, 0, 0, 0, 0, 0, 0, 1]), Tcp(12345u16))));
+
+        assert!(addr_is_reachable(&multiaddr!(Ip4([192, 168, 0, 1]), Tcp(12345u16))));
+        assert!(addr_is_reachable(&multiaddr!(Ip4([10, 0, 0, 1]), Tcp(12345u16))));
+        assert!(addr_is_reachable(&multiaddr!(Ip4([172, 16, 0, 1]), Tcp(12345u16))));
     }
 }
