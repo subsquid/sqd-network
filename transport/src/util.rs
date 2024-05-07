308fa183327e320b0d98b69a480b543030d456bd
Adam Wierzbicki <a.wierzbicki@subsquid.io>
2024-05-07 11:46:28 +0200

    Revert keypair encoding change (not backwards compatible)

--- transport/src/util.rs
+++ transport/src/util.rs
@@ -1,3 +1,4 @@
+use libp2p::identity::ed25519;
 use libp2p::{identity::Keypair, multiaddr::Protocol, Multiaddr};
 use std::path::PathBuf;
 
@@ -14,18 +15,18 @@ pub async fn get_keypair(path: Option<PathBuf>) -> anyhow::Result<Keypair> {
     match tokio::fs::metadata(&path).await {
         Ok(meta) if meta.is_file() => {
             log::info!("Reading key from {}", path.display());
-            let content = tokio::fs::read(&path).await?;
-            let keypair = Keypair::from_protobuf_encoding(&content)?;
-            Ok(keypair)
+            let mut content = tokio::fs::read(&path).await?;
+            let keypair = ed25519::Keypair::try_from_bytes(content.as_mut_slice())?;
+            Ok(keypair.into())
         }
         Ok(_) => {
             anyhow::bail!("Path exists and is not a file")
         }
         Err(_) => {
             log::info!("Generating new key and saving into {}", path.display());
-            let keypair = Keypair::generate_ed25519();
-            tokio::fs::write(&path, keypair.to_protobuf_encoding()?).await?;
-            Ok(keypair)
+            let keypair = ed25519::Keypair::generate();
+            tokio::fs::write(&path, keypair.to_bytes()).await?;
+            Ok(keypair.into())
         }
     }
 }
