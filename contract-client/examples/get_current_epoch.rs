3844ca9b55c227333a2a8f6c0c98697b795ff913
Adam Wierzbicki <a.wierzbicki@subsquid.io>
2024-07-30 15:20:30 +0200

    Contract utils/examples

--- contract-client/examples/get_current_epoch.rs
+++ contract-client/examples/get_current_epoch.rs
@@ -12,10 +12,12 @@ struct Cli {
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     SimpleLogger::new().with_level(log::LevelFilter::Info).env().init()?;
-    let cli = Cli::parse();
+    let cli: Cli = Cli::parse();
 
     let client = contract_client::get_client(&cli.rpc).await?;
-    let peer_ids = client.active_gateways().await?;
-    peer_ids.iter().for_each(|id| println!("{id}"));
+    let epoch_num = client.current_epoch().await?;
+    let epoch_start = client.current_epoch_start().await?;
+    let elapsed = epoch_start.elapsed()?;
+    log::info!("Current epoch is {epoch_num} started {} sec ago", elapsed.as_secs());
     Ok(())
 }
