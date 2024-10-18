69169a183a65aedae5d34f84ceb88af4c08b5335
Dzmitry Kalabuk <d.kalabuk@subsquid.io>
2024-10-18 11:33:34 +0300

    Change protobuf messages to v2.0.0
    Despite the messages remaining compatible on the wire,
    it's logically a breaking change for:
    - queries
    - query results
    - worker logs
    - client logs
    
    Pings are extended to allow the code to use both versions
    
    See https://github.com/subsquid/wiki/blob/network-rfc/network/rfc/04_network_communication.md

--- crates/messages/src/signatures.rs
+++ crates/messages/src/signatures.rs
@@ -5,79 +5,148 @@ use libp2p::{
     PeerId,
 };
 
-use crate::{Ping, ProstMsg, Query, QueryExecuted};
+use crate::{
+    query_finished, query_result, ProstMsg, Query, QueryExecuted, QueryFinished, QueryOk, QueryResult, QueryResultSummary
+};
 
-pub fn msg_hash<M: ProstMsg>(msg: &M) -> Vec<u8> {
-    let mut result = [0u8; 32];
+fn sha3_256(msg: &[u8]) -> [u8; 32] {
     let mut hasher = Sha3_256::default();
-    hasher.update(msg.encode_to_vec().as_slice());
-    Digest::finalize_into(hasher, result.as_mut_slice().into());
-    result.to_vec()
+    hasher.update(msg);
+    hasher.finalize().into()
 }
 
-fn verify_signature<T: SignedMessage>(peer_id: &PeerId, msg: &mut T) -> bool {
-    let sig = msg.detach_signature();
-    let encoded = msg.encode_to_vec();
-    let result = match PublicKey::try_decode_protobuf(&peer_id.to_bytes()[2..]) {
-        Ok(pubkey) => pubkey.verify(&encoded, &sig),
+pub fn msg_hash<M: ProstMsg>(msg: &M) -> [u8; 32] {
+    sha3_256(&msg.encode_to_vec())
+}
+
+fn sign(msg: &[u8], keypair: &Keypair) -> Vec<u8> {
+    keypair.sign(msg).expect("signing should succeed for Ed25519")
+}
+
+fn verify_signature(peer_id: PeerId, msg: &[u8], signature: &[u8]) -> bool {
+    match PublicKey::try_decode_protobuf(&peer_id.to_bytes()[2..]) {
+        Ok(pubkey) => pubkey.verify(msg, signature),
         Err(_) => false,
-    };
-    msg.attach_signature(sig);
-    result
-}
-
-pub trait SignedMessage: ProstMsg + Sized {
-    fn detach_signature(&mut self) -> Vec<u8>;
-    fn attach_signature(&mut self, signature: Vec<u8>);
-
-    fn sign(&mut self, keypair: &Keypair) {
-        _ = self.detach_signature(); // To make signing idempotent
-        let bytes = self.encode_to_vec();
-        let signature = keypair.sign(&bytes).expect("infallible for Ed25519");
-        self.attach_signature(signature);
-    }
-
-    fn verify_signature(&mut self, peer_id: &PeerId) -> bool {
-        verify_signature(peer_id, self)
     }
 }
 
-impl SignedMessage for Ping {
-    fn detach_signature(&mut self) -> Vec<u8> {
-        std::mem::take(&mut self.signature)
+impl Query {
+    pub fn sign(&mut self, keypair: &Keypair, worker_id: PeerId) {
+        let msg = self.signed_msg(worker_id);
+        self.signature = sign(&msg, keypair);
     }
 
-    fn attach_signature(&mut self, signature: Vec<u8>) {
-        self.signature = signature;
-    }
-}
-
-impl SignedMessage for Query {
-    fn detach_signature(&mut self) -> Vec<u8> {
-        std::mem::take(&mut self.signature)
+    pub fn verify_signature(&self, signer_id: PeerId, worker_id: PeerId) -> bool {
+        verify_signature(signer_id, &self.signed_msg(worker_id), &self.signature)
     }
 
-    fn attach_signature(&mut self, signature: Vec<u8>) {
-        self.signature = signature;
-    }
-}
-
-impl SignedMessage for QueryExecuted {
-    fn detach_signature(&mut self) -> Vec<u8> {
-        std::mem::take(&mut self.signature)
-    }
-
-    fn attach_signature(&mut self, signature: Vec<u8>) {
-        self.signature = signature;
-    }
-
-    fn verify_signature(&mut self, peer_id: &PeerId) -> bool {
-        if !verify_signature(peer_id, self) {
-            return false;
+    fn signed_msg(&self, worker_id: PeerId) -> Vec<u8> {
+        let mut msg = Vec::with_capacity(525000);
+        msg.extend_from_slice(self.query_id.as_bytes());
+        msg.extend_from_slice(&worker_id.to_bytes());
+        msg.extend_from_slice(&self.timestamp_ms.to_le_bytes());
+        msg.extend_from_slice(self.dataset.as_bytes());
+        msg.extend_from_slice(self.query.as_bytes());
+        if let Some(range) = self.block_range {
+            msg.extend_from_slice(&range.begin.to_le_bytes());
+            msg.extend_from_slice(&range.end.to_le_bytes());
         }
+        msg
+    }
+}
+
+impl QueryResultSummary {
+    pub fn new(result: &[u8]) -> Self {
+        let hash = sha3_256(result).to_vec();
+        Self {
+            hash,
+            size: result.len() as u64,
+            signature: None,
+        }
+    }
+
+    pub fn sign(&mut self, keypair: &Keypair, query_id: &str) {
+        let msg = Self::signed_msg(&self.hash, query_id);
+        self.signature = Some(sign(&msg, keypair));
+    }
+
+    pub fn verify_signature(&self, signer_id: PeerId, query_id: &str) -> bool {
+        if let Some(signature) = &self.signature {
+            verify_signature(signer_id, &Self::signed_msg(&self.hash, query_id), signature)
+        } else {
+            false
+        }
+    }
+
+    fn signed_msg(hash: &[u8], query_id: &str) -> Vec<u8> {
+        let mut msg = Vec::with_capacity(32 + query_id.len());
+        msg.extend_from_slice(hash);
+        msg.extend_from_slice(query_id.as_bytes());
+        msg
+    }
+}
+
+impl QueryOk {
+    pub fn new(result: Vec<u8>, last_block: u64) -> Self {
+        Self {
+            summary: Some(QueryResultSummary::new(&result)),
+            data: result,
+            last_block: Some(last_block),
+        }
+    }
+
+    pub fn sign(&mut self, keypair: &Keypair, query_id: &str) {
+        if let Some(summary) = &mut self.summary {
+            summary.sign(keypair, query_id);
+        }
+    }
+
+    pub fn verify_signature(&self, signer_id: PeerId, query_id: &str) -> bool {
+        if let Some(summary) = &self.summary {
+            summary.verify_signature(signer_id, query_id)
+        } else {
+            false
+        }
+    }
+}
+
+impl QueryResult {
+    pub fn sign(&mut self, keypair: &Keypair) {
+        if let Some(query_result::Result::Ok(result)) = &mut self.result {
+            result.sign(keypair, &self.query_id);
+        }
+    }
+
+    pub fn verify_signature(&self, signer_id: PeerId) -> bool {
+        if let Some(query_result::Result::Ok(result)) = &self.result {
+            result.verify_signature(signer_id, &self.query_id)
+        } else {
+            true
+        }
+    }
+}
+
+impl QueryFinished {
+    pub fn verify_signature(&self) -> bool {
+        if let Some(query_finished::Result::Ok(result)) = &self.result {
+            let Ok(worker_id) = self.worker_id.parse() else {
+                return false;
+            };
+            result.verify_signature(worker_id, &self.query_id)
+        } else {
+            true
+        }
+    }
+}
+
+impl QueryExecuted {
+    pub fn verify_client_signature(&self, worker_id: PeerId) -> bool {
+        let Some(query) = &self.query else {
+            return false;
+        };
         let Ok(client_id) = self.client_id.parse() else {
             return false;
         };
-        self.query.as_mut().is_some_and(|q| verify_signature(&client_id, q))
+        query.verify_signature(client_id, worker_id)
     }
 }
