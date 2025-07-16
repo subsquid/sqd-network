097fe33f798fd867afda72332b6773ed2f2fe12e
Dzmitry Kalabuk <d.kalabuk@subsquid.io>
2025-07-16 10:32:35 +0300

    Save the generated key to the specified dir

--- crates/keygen/src/main.rs
+++ crates/keygen/src/main.rs
@@ -1,21 +1,22 @@
+use libp2p::identity::{ed25519, Keypair};
 use std::path::PathBuf;
 
 use clap::Parser;
 
-use sqd_network_transport::util;
-
 #[derive(Parser)]
 #[command(version)]
 struct Cli {
-    /// Path to the generated key
-    filename: PathBuf,
+    /// Path to the directory where the key will be saved
+    path: PathBuf,
 }
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    let filename = Cli::parse().filename;
-    let keypair = util::get_keypair(Some(filename)).await?;
-    let peer_id = keypair.public().to_peer_id();
+fn main() -> anyhow::Result<()> {
+    let mut path = Cli::parse().path;
+    let keypair = ed25519::Keypair::generate();
+    let bytes = keypair.to_bytes();
+    let peer_id = Keypair::from(keypair).public().to_peer_id();
+    path.push(peer_id.to_base58());
+    std::fs::write(path, bytes)?;
     println!("{peer_id}");
     Ok(())
 }
